ad64cdd6b5b8d3bce464c6958bb23da05504e9b5
Bryant-Wang24 <485969746@qq.com>
2023-04-18 15:36:46 +0800

    监听ctrl+c中断信号

--- main.go
+++ main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"os"
 	"os/exec"
+	"os/signal"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	go func() { // 开启一个gin协程，防止阻塞调起chrome
-		gin.SetMode(gin.ReleaseMode)
+		gin.SetMode(gin.DebugMode)
 		router := gin.Default()
 		router.GET("/", func(c *gin.Context) {
 			c.Writer.Write([]byte("a test page"))
@@ -18,5 +20,10 @@ func main() {
 	chromePath := "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
 	cmd := exec.Command(chromePath, "--app=http://127.0.0.1:8080/")
 	cmd.Start()
-	select {}
+	chSignal := make(chan os.Signal, 1)
+	signal.Notify(chSignal, os.Interrupt)
+	select {
+	case <-chSignal:
+		cmd.Process.Kill()
+	}
 }
