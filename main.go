86709654748a7fa569e7c82f83ad2408c620aa40
Bryant-Wang24 <485969746@qq.com>
2023-04-22 18:20:05 +0800

    关闭chrome时，退出后台服务

--- main.go
+++ main.go
@@ -11,14 +11,27 @@ import (
 var Port = "27149"
 
 func main() {
+	chChromeDie := make(chan struct{})
 	go server.Run(Port)
-	chromePath := "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
-	cmd := exec.Command(chromePath, "--app=http://127.0.0.1:"+Port+"/static")
-	cmd.Start()
-	chSignal := make(chan os.Signal, 1)
-	signal.Notify(chSignal, os.Interrupt)
+	go startBrowser(chChromeDie)
+	chSignal := listenToInterrupt()
 	select {
 	case <-chSignal:
-		cmd.Process.Kill()
+	case <-chChromeDie:
+		os.Exit(0)
 	}
+}
+
+func startBrowser(chChromeDie chan struct{}) {
+	chromePath := "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+	cmd := exec.Command(chromePath, "--app=http://127.0.0.1:"+Port+"/static/index.html")
+	cmd.Start()
+	cmd.Wait()
+	chChromeDie <- struct{}{}
+}
+
+func listenToInterrupt() chan os.Signal {
+	chSignal := make(chan os.Signal, 1)
+	signal.Notify(chSignal, os.Interrupt)
+	return chSignal
 }
